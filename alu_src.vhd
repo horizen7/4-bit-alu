b6b9b156042d20550eaab1988f6d099d1318d9b8
horizen7 <davidro7777@gmail.com>
2025-08-26 16:44:27 -0500

    merged

--- alu_src.vhd
+++ alu_src.vhd
@@ -1,36 +1,82 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity alu is
     Port (
             opA, opB, opCode : in std_logic_vector(3 downto 0);
             output : out std_logic_vector(3 downto 0);
-            mode : in std_logic;
+            mode : in std_logic; -- '0' = unsigned | '1' = signed
             less_than, great_than, equal : out std_logic;
-            /* 
-               N - negative
-               Z - zero
-               C - carry
-               V - overflow
-            */
+             
+            --   N - negative
+            --   Z - zero
+            --   C - carry
+            --   V - overflow
+            
+            N : out std_logic;
             Z : out std_logic;
             C : out std_logic;
-            V : out std_logic;
-            N : out std_logic
+            V : out std_logic
     );
 end alu;
 
 architecture arch of alu is
     -- making custom type
-    type opcode_t is (add_t, sub_t, and_t, or_t, xor_t, not_t, comp_t);
+    
+    type opcode_t is (add_t, sub_t, mult_t, div_t, and_t, or_t, xor_t, not_t, comp_t);
     signal op : opcode_t;
-    signal tmp_out : unsigned(4 downto 0);
+    signal a_u, b_u : unsigned(3 downto 0);
+    signal a_s, b_s : signed(3 downto 0);
+    signal tmp_out : std_logic_vector(4 downto 0); -- one bit longer to detect overflow
 begin
-    with op select
-        
-    with 
+    -- mapping the enum
+    op <= opcode_t'val( to_integer(unsigned(opCode)) );
 
+    -- mapping internal operands
+    a_u <= unsigned(opA); b_u <= unsigned(opB);
+    a_s <= signed(opA); b_s <= signed(opB);
 
-            
+    result_proc : process (op, a_u, a_s, b_u, b_s, mode)
+    begin
 
-            
+        -- defaults first
+        tmp_out <= (others => '0');
+
+        case op is
+            when add_t =>
+                if mode = '0' then
+                    -- append '0' bit to capture carry for C flag
+                    tmp_out <= std_logic_vector(('0' & a_u) + ('0' & b_u));
+                else
+                    tmp_out <= std_logic_vector(resize(a_s, 5) + resize(b_s, 5));
+                end if;
+            when others => null;
+        end case;
+    end process result_proc;
+
+    -- split the process between flag and result because it looks better 
+
+    flag_proc : process (op, a_u, a_s, b_u, b_s, mode, tmp_out)
+    begin
+
+        N <= '0'; Z <= '0'; C <= '0'; V <= '0';
+
+        case op is
+            when add_t =>
+                if mode = '0' then
+                    C <= tmp_out(4);
+                else
+                    if (a_s(3) = b_s(3)) and (tmp_out(3) /= a_s(3)) then
+                        V <= '1';
+                    else
+                        V <= '0';
+                    end if;
+                end if;
+            when others => null;
+        end case;
+    end process flag_proc;
+
+    output <= tmp_out(3 downto 0);
+    
+end arch;
--- alu_tb.vhd
+++ alu_tb.vhd
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -64,4 +65,72 @@ begin
         opA <= "0111"; opB <= "0001"; wait for 20 ns;
 
     end process;
+=======
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity alu_tb is
+end alu_tb;
+
+architecture test of alu_tb is
+    signal opA, opB, opCode : std_logic_vector(3 downto 0) := "0000";
+    signal output : std_logic_vector(3 downto 0);
+    signal mode : std_logic := '0'; -- '0' = unsigned | '1' = signed
+    signal less_than, great_than, equal : std_logic;
+    signal N, Z, C, V : std_logic;
+
+    component alu
+        Port (
+                opA, opB, opCode : in std_logic_vector(3 downto 0);
+                output : out std_logic_vector(3 downto 0);
+                mode : in std_logic;
+                less_than, great_than, equal : out std_logic;
+                N, Z, C, V : out std_logic
+        );
+    end component;
+
+begin
+    uut : alu port map (
+        opA => opA,
+        opB => opB,
+        opCode => opCode,
+        output => output,
+        mode => mode,
+        less_than => less_than,
+        great_than => great_than,
+        equal => equal,
+        N => N,
+        Z => Z,
+        C => C,
+        V => V
+    );
+
+    process
+    begin
+
+        -- setting to ADD
+        opCode <= "0000";
+
+        
+        --    1) Add unsigned values, no carry
+        --    2) Add unsigned, carry
+        --    3) Signed no overflow
+        --    4) Signed with overflow
+        
+        -- step 1
+        opA <= "0001"; opB <= "0001"; wait for 20 ns;
+
+        -- step 2
+        opA <= "1111"; opB <= "1110"; wait for 20 ns;
+
+        -- step 3
+        mode <= '1';
+        opA <= "0011"; opB <= "0001"; wait for 20 ns;
+
+        -- step 4
+        opA <= "0111"; opB <= "0001"; wait for 20 ns;
+
+    end process;
+>>>>>>> a5cb29f (hi)
 end test;
